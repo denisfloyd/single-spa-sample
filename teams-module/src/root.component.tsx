5076e40e018b203c0405291bec3db3a9fc4af45d
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-06-30 20:12:15 -0300

    style: teams page style

--- teams-module/src/root.component.tsx
+++ teams-module/src/root.component.tsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
 import { ListUsers } from "@df/users-module";
 
-export default function Root(props) {
-  return <section><ListUsers /></section>;
+export const Select = styled.select`
+  height: 40px;
+  width: 10rem;
+`;
+
+export default function Root() {
+  return (
+    <section>
+      <h1>Teams</h1>
+
+      <Select>
+        <option value="">Select a teams</option>
+        <option value="team-1">Team 1</option>
+        <option value="team-2">Team 2</option>
+      </Select>
+
+      <ListUsers />
+    </section>
+  );
 }
